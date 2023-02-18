ee1eac3af288f2872ba5f5c60f4efdf0149c85d4
kjunegeun <junegeun.kim@gmail.com>
2023-02-18 14:24:27 +0900

    프로젝트 생성

---
+++ src/main/java/me/june/bggcommunity/BggCommunityApplication.java
@@ -0,0 +1,13 @@
+package me.june.bggcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BggCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BggCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/june/bggcommunity/BggCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package me.june.bggcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BggCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
