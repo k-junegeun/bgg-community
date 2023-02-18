5924a06c4f3bc0cd0e2352313fa8e6257ff64580
kjunegeun <junegeun.kim@gmail.com>
2023-02-18 15:29:28 +0900

    cd 환경 구축

---
+++ src/main/java/me/june/bggcommunity/WelcomeController.java
@@ -0,0 +1,14 @@
+package me.june.bggcommunity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
